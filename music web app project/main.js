b0be8914875e0f740bfe88cf8ddc9a8d72fe2e04
FedoraStudios <82604558+FedoraStudios@users.noreply.github.com>
2021-12-10 12:48:50 -0500

    update 2

--- music web app project/main.js
+++ music web app project/main.js
@@ -1,8 +1,17 @@
-song = ""
+song = "";
+song2 = "";
+
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
 
 function preload()
 {
-  song = loadSound(music.mp3);
+  song = loadSound("music.mp3");
+  song2 = loadSound("music1.mp3")
 }
 
 function setup()
@@ -11,10 +20,34 @@ function setup()
   canvas.center();
 
   video = createCapture(VIDEO);
-  video.hide
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on("pose", gotPoses);
+}
+
+function gotPoses(result)
+{
+  if(result > 0)
+  {
+    console.log(result);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY)
+
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY)
+  }
+}
+
+function modelLoaded()
+{
+  console.log("PoseNet Is Initialized");
 }
 
 function draw()
 {
-    image(video, 0, 0, 600, 500)
+  image(video, 0, 0, 600, 500);
 }
